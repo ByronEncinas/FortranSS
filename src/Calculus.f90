bde9aa02d0ebcecd755b0cf50f252560a8054348
ByronEncinas <byron_encinas@outlook.com>
2023-10-05 22:04:50 -0500

    Calculus-OOP-Integration-update

--- src/Calculus.f90
+++ src/Calculus.f90
@@ -1,4 +1,4 @@
-Module calculus
+Module Calculus
     
     use iso_fortran_env, only: real32, real64, real128
 
@@ -16,32 +16,37 @@ Module calculus
 
 contains
 
-Subroutine Euler_Method(self, func, delta, ab, Integral)
+Subroutine Euler_Method(self, func, ab, delta)
 
     class(Integrate), intent(in out) :: self
-    integer:: i, n
-    real, optional, intent(inout) :: delta
-    real, external :: func
-    real, intent(out) :: Integral
-    real, intent(in), dimension(2) :: ab
-    real:: xi
+    integer(kind=real32):: i, n
+    real(kind=real32), intent(inout) :: delta
+    real(kind=real32), external :: func
+    real(kind=real32), intent(in), dimension(2) :: ab
+    real(kind=real32):: xi
 
-    if( .not. present(delta) ) delta = 1e-6_real32 ! default value
+    write(*,*) "Here is Euler Method"
 
-    Integral = 0
+    !if( .not. present(delta) ) delta = 1.0e-6_real32 ! default value
+
+    write(*,*) delta
+
+    self%Integral = 0
     n = floor((ab(2) - ab(1))/delta) 
     
+    write(*,*) n
 
     Do i = 1, n, 1 ! for midpoints
-        xi = ab(1) + delta/2
-        Integral = Integral + func(xi )*delta
+
+        xi = ab(1) + i*delta/2
+        write(*,*) self%Integral
+        self%Integral = self%Integral + func(xi)*delta
+    
     End do
 
-    self%Integral =  (Integral )*delta
-
+    self%Integral =  (self%Integral)*delta
 
 End Subroutine Euler_Method
-
 
 Subroutine Simpson_Method(self, func, delta, ab, Integral, type)
     
@@ -60,6 +65,8 @@ Subroutine Simpson_Method(self, func, delta, ab, Integral, type)
 
     if (type == '1/3') Then
 
+        write(*,*) "Here is Simpsons Method 1/3"
+
         Integral = 0
         n = floor((ab(2) - ab(1))/delta/2) 
 
@@ -74,11 +81,11 @@ Subroutine Simpson_Method(self, func, delta, ab, Integral, type)
 
     else if (type == '1/8') Then
 
-        
+        write(*,*) "Here is Simpsons Method 1/8"
 
     else if (type == '3/8') Then
 
-        
+        write(*,*) "Here is Simpsons Method 3/8"
 
     end if
 
