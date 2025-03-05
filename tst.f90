6e71b1038419171776b3f78e33b8388765090ef4
ByronEncinas <byron_encinas@outlook.com>
2025-03-05 14:04:10 -0700

    memory leak, moved to tst.f90

--- tst.f90
+++ tst.f90
@@ -21,17 +21,7 @@ program main
     dim = 4  ! Size of the matrix
 
     ! Initialize matrix A
-    call A%init(dim, 0)
-
-    A%M(1,1) = 1.0
-    A%M(1,2) = 0.0
-    A%M(1,3) = 0.0
-    A%M(2,1) = 0.0
-    A%M(2,2) = 1.0
-    A%M(2,3) = 0.0
-    A%M(3,1) = 0.0
-    A%M(3,2) = 0.0
-    A%M(3,3) = 1.0
+    call A%init(dim)
 
     ! Print the matrix
     print *, "Matrix A:"
@@ -45,4 +35,29 @@ program main
     ! Print result
     print *, "Determinant of A:", det
     
+
+    ! Test for Calculus Class
+
+    interval = [0.0_real32,1.0_real32]
+    
+    call Integration%Euler(func, interval, delta)
+    write(*,*)
+    call Integration%Simpson(func, interval, "1/3", delta)
+    write(*,*)
+    call Integration%Simpson(func, interval, "3/8", delta)
+    write(*,*)
+    call Flux%Diff(func, 0.0_real32, delta)
+    write(*,*)
+    call Flux%SecDiff(func, 0.0_real32, delta)
+
+
+contains
+
+real function func(x) result(res) ! output
+    real(kind=real32), intent (in) :: x ! input
+
+    res = x**2.
+
+end function
+
 end program main
